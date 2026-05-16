6231639f47353465168c11d1f8a58bc74a37b83a
7azem512 <hazemsaed5122@gmail.com>
2026-05-16 16:02:39 +0300

    Initialize FitLink project

---
+++ src/main/java/com/project/FitLink/FitLinkApplication.java
@@ -0,0 +1,13 @@
+package com.project.FitLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/FitLink/FitLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.FitLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
